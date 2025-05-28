7abc1d5edb78d735461cfbd69db0c336ef3164b0
bestsoft100 <163331759+Bestsoft101@users.noreply.github.com>
2025-05-28 23:27:27 +0200

    Updater now works in MultiMC

--- src/b100/installer/updater/Updater.java
+++ src/b100/installer/updater/Updater.java
@@ -10,6 +10,8 @@ import java.util.zip.ZipFile;
 
 import b100.installer.Download;
 import b100.installer.Global;
+import b100.installer.util.MultiMCHelper;
+import b100.json.JsonParser;
 import b100.json.element.JsonArray;
 import b100.json.element.JsonObject;
 
@@ -20,44 +22,116 @@ public class Updater {
 	public static void update(String[] args) {
 		System.out.println("Update !!!");
 		
-		String javaExe = getEscapedPath(new File(System.getProperty("java.home"), "bin/java"));
-		System.out.println("Java Path: " + javaExe);
+		File runInstance = MultiMCHelper.getRunInstanceDirectory();
+		System.out.println("Run Instance: " + runInstance);
 		
+		// When installing the updater as a jarmod in MultiMC, we have to replace the jarmod file,
+		// not the one that's currently running, because MultiMC creates a temporary file
+		// when the instance is started
+		if(runInstance != null) {
+			File updaterJar = findUpdaterJarInInstance(runInstance);
+			if(updaterJar != null) {
+				installUpdate(updaterJar, false);
+				return;
+			}
+		}
+		
+		installUpdate(getJarFileContainingClass(Updater.class), true);
+	}
+	
+	private static void downloadUpdate(File file) {
 		String jarUrl = Global.getDownloadUrl() + "bta-installer/installer.jar";
-		File tempFile = new File("installer.jar.tmp");
 		
-		if(tempFile.exists()) {
-			tempFile.delete();
+		if(file.exists()) {
+			file.delete();
 		}else {
-			File parent = tempFile.getAbsoluteFile().getParentFile();
+			File parent = file.getAbsoluteFile().getParentFile();
 			if(!parent.exists()) {
 				parent.mkdirs();
 			}
 		}
 		
 		System.out.println("Downloading update...");
-		new Download(jarUrl).downloadIntoFile(tempFile);
+		new Download(jarUrl).downloadIntoFile(file);
+	}
+	
+	private static File findUpdaterJarInInstance(File instance) {
+		System.out.println("Install update in instance: " + instance.getAbsolutePath());
 		
-		File currentFile = getJarFileContainingClass(Updater.class);
+		File mmcPackFile = new File(instance, "mmc-pack.json");
+		File jarmodsFolder = new File(instance, "jarmods");
+		
+		JsonObject mmcPack = JsonParser.instance.parseFileContent(mmcPackFile);
+		JsonArray components = mmcPack.getArray("components");
+		
+		for(int i=0; i < components.length(); i++) {
+			String uid = components.get(i).getAsObject().getString("uid");
+			if(uid.startsWith("org.multimc.jarmod.")) {
+				String jarmodId = uid.substring(19);
+				
+				System.out.println("Found jar mod with id: " + jarmodId);
+				
+				File jarmodFile = new File(jarmodsFolder, jarmodId + ".jar");
+				if(isUpdaterJar(jarmodFile)) {
+					return jarmodFile;
+				}
+			}
+		}
+		return null;
+	}
+	
+	private static void installUpdate(File currentFile, boolean startProcess) {
+		File updateFile = new File("installer.jar.tmp");
+		File backupFile = new File("installer.jar.old");
+		
 		System.out.println("Current jar file: " + currentFile.getAbsolutePath());
+		System.out.println("Downloading update: " + updateFile.getAbsolutePath());
+		downloadUpdate(updateFile);
 		
-		File oldFile = new File("installer.jar.old");
-		copyFile(currentFile, oldFile);
+		if(startProcess) {
+			copyFile(currentFile, backupFile);
+			
+			// Start another process that will delete the currently running jar file and put the updated file in its place.
+			// This needs to be done in a different process, because it's not possible to delete or rename a jar file that's currently running.
+			
+			String javaExe = getEscapedPath(new File(System.getProperty("java.home"), "bin/java"));
+			System.out.println("Java Path: " + javaExe);
+			
+			List<String> runArgs = new ArrayList<String>();
+			runArgs.add(javaExe);
+			runArgs.add("-cp");
+			runArgs.add(getEscapedPath(backupFile));
+			runArgs.add(RenameFileAndRun.class.getName());
+			runArgs.add("--from");
+			runArgs.add(getEscapedPath(updateFile));
+			runArgs.add("--to");
+			runArgs.add(getEscapedPath(currentFile));
+			
+			Process process = startProcess(runArgs);
+			System.out.println("Started process: " + process);
+			System.exit(0);
+			return;
+		}
 		
-		// Start another process that will delete the currently running jar file and put the updated file in its place.
-		// This needs to be done in a different process, because it's not possible to delete or rename a jar file that's currently running.
-		List<String> runArgs = new ArrayList<String>();
-		runArgs.add(javaExe);
-		runArgs.add("-cp");
-		runArgs.add(getEscapedPath(oldFile));
-		runArgs.add(RenameFileAndRun.class.getName());
-		runArgs.add("--from");
-		runArgs.add(getEscapedPath(tempFile));
-		runArgs.add("--to");
-		runArgs.add(getEscapedPath(currentFile));
-		Process process = startProcess(runArgs);
-		System.out.println("Started process: " + process);
-		System.exit(0);
+		boolean success = true;
+		if(backupFile.exists() && !backupFile.delete()) {
+			System.out.println("Could not delete file: " + backupFile.getAbsolutePath());
+			success = false;
+		}
+		if(!currentFile.renameTo(backupFile)) {
+			System.out.println("Could not rename current file: " + currentFile.getAbsolutePath());
+			success = false;
+		}
+		if(!updateFile.renameTo(currentFile)) {
+			System.out.println("Could not rename update file: " + updateFile.getAbsolutePath());
+			success = false;
+		}
+		
+		if(success) {
+			System.out.println("Update complete!");	
+		}else {
+			System.out.println("Update failed!");
+		}
 	}
 	
 	public static List<String> searchAvailableUpdates() {
